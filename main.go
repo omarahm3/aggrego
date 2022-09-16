dc3e518cd9ebb303abe45816f39206d3f324b16d
Omar Ahmed <omarking05@gmail.com>
2022-09-16 15:06:00 +0200

    feat(main): add PORT env variable

--- main.go
+++ main.go
@@ -4,21 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
-const PORT = 8888
+var DEFAULT_PORT = "8080"
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = DEFAULT_PORT
+	}
+
 	s := newServer()
 	go s.run()
 
-	l, err := net.Listen("tcp", fmt.Sprintf(":%d", PORT))
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
 		log.Fatalf("unable to start tcp server: %s", err.Error())
 	}
 
 	defer l.Close()
-	log.Printf("server started at :%d", PORT)
+	log.Printf("server started at :%s", port)
 
 	for {
 		conn, err := l.Accept()
